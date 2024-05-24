42e8d79063918b98841543abc337715f1dc0cd27
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2024-05-24 02:10:11 -0300

    Adding an initial support for the Catarina-A2 satellite

--- spacelab_decoder/spacelabdecoder.py
+++ spacelab_decoder/spacelabdecoder.py
@@ -77,6 +77,7 @@ _SATELLITES                     = [["FloripaSat-1", "floripasat-1.json"],
                                    ["GOLDS-UFSC", "golds-ufsc.json"],
                                    ["Aldebaran-1", "aldebaran-1.json"],
                                    ["Catarina-A1", "catarina-a1.json"],
+                                   ["Catarina-A2", "catarina-a2.json"],
                                    ["SpaceLab-Transmitter", "spacelab-transmitter.json"]]
 
 _PROTOCOL_NGHAM                 = "NGHam"
